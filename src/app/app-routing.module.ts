8900417d6c99838e152ca0166a071c0b2a513956
kanomdook <kanung.2535@gmail.com>
2018-11-06 12:05:44 +0700

    [add] login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'chat-detail', loadChildren: './pages/chat-detail/chat-detail.module#ChatDetailPageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,3 +1,4 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  credencial: any = {};
+  users: Array<any> = [];
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.users = [{
+      _id: '1',
+      display: 'ร้านค้าเสื้อผ้ามือสอง',
+      img: 'https://stickershop.line-scdn.net/stickershop/v1/product/444/LINEStorePC/main@2x.png;compress=true',
+      username: 'dook',
+      password: '1234'
+    },
+    {
+      _id: '2',
+      display: 'Apple Store',
+      img: 'https://i.imgur.com/QiAMG0g.jpg',
+      username: 'apple',
+      password: '1234'
+    },
+    {
+      _id: '3',
+      display: 'ร้านเฟอร์นิเจอร์',
+      img: 'https://upscaleresalefurnishings.com/wp-content/uploads/2015/10/chair.png',
+      username: 'furniture',
+      password: '1234'
+    }];
+  }
+
+  login() {
+    const userItem = this.users.filter(user => {
+      return this.credencial.username === user.username && this.credencial.password === user.password;
+    });
+    if (userItem.length > 0) {
+      localStorage.setItem('user', JSON.stringify(userItem[0]));
+      this.navCtrl.navigateForward('home');
+    } else {
+      this.credencial.username = '';
+      this.credencial.password = '';
+    }
   }
 
 }
